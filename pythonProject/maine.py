4efbe5bf73df8166feb2603f0c43ceafe006ce7e
FrankOpsWins <chibunnaonwumere01@gmail.com>
2025-05-29 10:47:33 +0100

    secondary file

---
+++ pythonProject/maine.py
@@ -0,0 +1,407 @@
+# """letb = 6
+# b = 7
+# print (letb)
+#
+# print(type(letb))
+#
+# import random
+# # print (random.randrange(20,30))
+# # print (random.randrange(90,100))
+# #
+# # a="flammytech schools"
+# # print(a[14])
+# # print(a[15])
+# #
+# # print(range(1,10))
+#
+# for hand in range(1,10):
+#      print(hand)
+#
+#
+# '''for hand in "easter break":
+#     print(hand)
+#
+# hand = "easter break"
+# print(len(hand))'''
+#
+# '''b="today is awesome"
+# print(b[10:16])
+#
+# a=[1,2,3,4,5,6,7,8,9,10]
+# print(a[1: :2])
+#
+# a=input("what is your name ")
+# print("welcome", a)'''
+#
+# '''a= input("what is the first number ")
+# b= input("what is the last number ")
+# a = int (a)
+# b = int (b)
+# if a>b:
+#     print (a)
+# else:
+#     print (b)
+#
+# print(a>b)'''
+#
+# '''x = [20,47,66,86,30, 12, 67, 12 ,90, 11, 99, 178,234,10]
+# x.sort()
+# print(x)
+# print(x[0])
+# print(x[-2])'''
+#
+# '''a = "today is a good day"
+# print(a.split("o"))
+#
+# a= "flamy"
+# b = "tech"
+# c = a +" " + b + " " + "adetola"
+# print(c)
+# '''
+#
+# '''x = [1,1,1,2,3,4,5,1,9,9,8,9]
+# x.sort()
+# x = set(x)
+# x = list(x)
+# print(x[-2])'''
+#
+# '''x = [0,1,2,3,4,5,6,7,8,9,10,-4,-14]
+# x.sort(reverse=True)
+# print(x)
+# print(x[0])
+# print(len(x))
+# print(x[0:11])
+# print(x[0: :2])'''
+#
+# '''x=[1,6,4,2,2,6,7,3,4,1]
+# x.sort(reverse=True)
+# print(x[-5: ])
+#
+# a=[1,2,4,5,6,7]
+# b=[1,6,4,2,2,6,7,3,4,1]
+# a.insert(2, 3)
+# a.insert(7,8)
+# a.extend(b)
+# a.append(10)
+# a.sort(reverse=False)
+# print(a)'''
+#
+# '''a=[1,2,3,4,5,6,7,8,9,10]
+# second = a[4:]
+# i = 0
+# while i < len(second):
+#     print(second[i])
+#     i += 1'''
+#
+# '''me = [6,7,2,9,13,8,12,1,0,3,3,4,8,11,3,6,7,99]
+# newlist = []
+# for x in me:
+#     x += 5
+#     newlist.append(x)
+# print(newlist)
+# '''
+# '''me.sort(reverse=False)
+# men = me[-7:]
+# i = 0
+# while i < len(men):
+#     print(men[i])
+#     i += 1'''
+#
+#
+# # victor = 0
+# # while victor < len(a):
+# #     print(a[victor])
+# #     victor += 1
+#
+# # for arr in a:
+# #     print(arr)
+#
+#
+# '''for boys in second:
+#     print(boys)'''
+#
+# #print(a[4:10])
+#
+# '''for hand in a:
+#     print(f'this is {hand}')'''
+# '''a.pop(5)
+# a.pop()
+# a.clear()
+# del a[2]
+# del a'''
+# #print(a)
+#
+# '''age = int(input('How old are you?'))
+# #age = int(age)
+# age += 5
+# txt = f"My name is john, I am {age}"
+# print(txt)'''
+#
+# price = 59
+# price = float(price)
+# txt = f"the price is {price} dollars"
+# print(txt)
+# #
+# # name = "dave"
+# # gender = "male"
+# # age = 15
+# # txt = f"your name is {name} and you are a {gender} of {age} years"
+# # print(txt)
+# #
+# # txt = 'we are the \'so-called vikings\' from the north'
+# # print(txt)
+# #
+# # print(10 > 9)
+# # print(10 == 9)
+# # print(10 < 90)
+# #
+# # x = 13//3
+# # print(x)
+#
+# '''a = (1,2,3,5,6)
+# a = list (a)
+# a.insert(3,4)
+# a.pop()
+# a= tuple (a)
+# print(a)
+# print(type(a))'''
+#
+# # mtlist = []
+# # for vivtor in range(11):
+# #      j = int(input(f"enter {vivtor} number "))
+# #      mtlist.append(j)
+# # mtlist.sort(reverse=False)
+# # print(mtlist)
+# # print(mtlist[0])
+# # print(mtlist[-1])
+# # print(type(mtlist[0]))"""
+# from itertools import count
+#
+# # for hand in range(1,11):
+# #      print(hand)
+#
+#
+#
+# #for hand in a:
+# #   print(f'this is {hand}')
+#
+# '''a = [1,12,2,3,4,5,6,7,8]
+# arr = 0
+# for me in a:
+#       arr-=1
+#       print(a[arr])'''
+# '''hand = 0
+# while hand < len(a):
+#
+#       hand -= 1
+#       print(a[hand])'''
+#
+# '''for a in range(10, 21):
+#       if a%2 == 0:
+#             print(a)'''
+#
+#
+# '''card = (1,2,3,4,5,6,7,8,9,10)
+# # a = card[0]
+# # b = card[1:9]
+# # c = card [9]
+# # print(a)
+# # print(b)
+# # print(c)
+# (a,*b,c) = card
+# print(a)
+# print(b)
+# print(c)'''
+#
+# '''thistuple = ("apple", "banana", "cherry")
+# for i in range(len(thistuple)):
+#   print(thistuple[i])'''
+#
+# # a = (1,2,3,1,6,5,2,3,1,4,5,6,1,2,3,6,7,6,1,2,3,2,1,1,2,1,3,6)
+# # b = set(((a)))
+# # print(b)
+# # a.count(1)
+# # a.index(2)
+# # print(a.count(1))
+# # print(a.index(7))
+#
+# # a = [1,2,3,4,5,6,7]
+# # b = [1,3,6,4,2,2,9]
+# # a.extend(b)
+# # print(a)
+# # c = set ((a))
+# # a = list (c)
+# # print(type(c))
+# # print(a)
+# #
+# # a = [1,2,3,[4,5,6,7,[8,9,10]],11,12]
+# # print(a[3][4][1])
+#
+# # a = {
+# #     "numbers": [0,1,2,[3,4,5,],6,7,[8],9],
+# #     "gender" : "female"
+# # }
+# # p= (a.get("numbers"))
+# # print(p[3][1])
+# #print(a["numbers"][3][1])
+#
+# # car = {
+# # "brand": "Ford",
+# # "model": "Mustang",
+# # "year": 1964
+# # }
+# #
+# # a = car.values()
+# # print(a) #before the change
+# #
+# # car["year"] = 2020
+# #
+# # print(a) #after the change
+#
+# # a = {"name":"victor","location":"lagos"}
+# # a["lastname"] = "maddison"
+# # print(a["lastname"])
+# # d = a["location"]
+# # print(d)
+# #
+# # for c in a:
+# #     print(a[c], ":" ,c)
+# #
+# # b  = a.keys()
+# # print(b)
+# # a["size"] = "slim"
+# # print(a)
+# # print(a["size"])
+# # for b in a:
+# #     print(b, " : ", a[b])
+# #
+# # for b in a.items():
+# #     print(b)
+#
+#
+# # a = {"first":10, "second":11, "third":12, "fifth":13}
+# # a.pop("fifth")
+# # a["sixth"] = 13
+# # print(a)
+# # for b, value in a.items():
+# #     if value > 10:
+# #         print(b, ":" ,value)
+# # for c in a:
+# #     if a[c] > 10:
+# #         print(c,":", a[c])
+#
+# # a = 1
+# # b = 2
+# # c = 3
+# # if a > b and a > c:
+# #     print ("a is the biggest")
+# # elif b > a and b > c:
+# #     print("b is the biggest")
+# # else:
+# #     print("c is the biggest")
+#
+# # a = int(input("enter your age "))
+# # b = int(input("enter your age "))
+# # c = int(input("enter your age "))
+# #
+# # if a>b and a>c:
+# #     print("a is the biggest")
+# # elif b>a and b>c:
+# #     print("b is the biggest")
+# # elif c>a and c>b:
+# #     print("c is the biggest")
+# # else:
+# #     print("they are equal")
+#
+# # a = "hello"
+# # b = "hell"
+# # c = "o"
+# # d = "hell" + "o"
+# # print(d)
+# # if a := d:
+# #     print(True)
+# #
+# # a = 20
+# # b = 33
+# # c = 500
+# # if a > b or a > c:
+# #   print("At least one of the conditions is True")
+#
+# # x = 11
+# #
+# # if x > 10:
+# #   print("Above ten,")
+# #   if x > 20:
+# #     print("and also above 20!")
+# #   else:
+# #     print("but not above 20.")
+#
+# # def frank(x):
+# #   print(x+x)
+# #
+# # frank(15)
+# #
+# # def david ():
+# #   print(2+2)
+# #
+# # david()
+# #
+# # def func(Name, Age):
+# #   print(f"Good Morning {Name}, you are {Age} years old")
+# #
+# # func("dave", 10)
+#
+# def adaka(a,b,c):
+#   if a>b and a>c:
+#     print(a)
+#   elif b>a and b>c:
+#     print(b)
+#   elif c>a and c>b:
+#     print(c)
+#   elif a == b and a>c:
+#     print(f'{a},{b} are biggest')
+#   elif b == c and b>a:
+#     print(f"{b}, {c} are biggest")
+#   elif a == c and a>b:
+#     print(f"{a},{c} are biggest")
+#   else:
+#     print("they are equal")
+#
+# adaka(2000, 21000,21000)
+
+
+
+# def vv():
+#   print(122)
+#   return
+#   print(333)
+#   print(890)
+# print('Dave')
+# vv()
+# print("victor")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
